996d7e9bd90dff4a665263e075b6cc8ba2b41c9c
Chris Forbes <u1151925@umail.usq.edu.au>
2023-11-13 11:34:47 +1000

    Created sections and updated App.jsx with structure

---
+++ src/sections/floating-nav/FloatingNav.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './floating-nav.css'
+
+const FloatingNav = () => {
+  return (
+    <section id="floating_nav"></section>
+  )
+}
+
+export default FloatingNav
---
+++ src/sections/navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './navbar.css'
+
+const Navbar = () => {
+  return (
+    <section id="nav_bar"></section>
+  )
+}
+
+export default Navbar
