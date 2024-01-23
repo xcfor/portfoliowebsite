252803a9ff80418fcc1f6630d1931577e148bd93
Chris Forbes <u1151925@umail.usq.edu.au>
2024-01-23 18:29:56 +1000

    Fixed Swiper imports

--- src/sections/testimonials/Testimonials.jsx
+++ src/sections/testimonials/Testimonials.jsx
@@ -1,13 +1,12 @@
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay } from "swiper";
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/autoplay"
 
 // import required modules
-import { Pagination } from "swiper";
+import { Pagination, Autoplay } from "swiper/modules";
 
 
 import testimonials from './data'
